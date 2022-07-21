bdc058265d3d85d87598200cf20d70e38257e89e
joan <https://github.com/wakander-joan/Teste.git>
2022-07-20 21:25:48 -0300

    Implemente response e requeste

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
@@ -4,5 +4,5 @@ import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
 
 public interface ClienteService {
-	ClienteResponse criaCliente(ClienteRequest clienteRequest)
+	ClienteResponse criaCliente(ClienteRequest clienteRequest);
 }
